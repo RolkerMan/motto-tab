f911fd93ed427ea69e826da81dfaa04f86795593
Rolker <email@example.com>
2023-07-23 17:32:40 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,18 @@
+var mottos = [
+  // https://smyachenkov.com/posts/book-review-the-staff-engineers-path/
+  {
+    content: 'Writing about engineering strategy is hard because good strategy is pretty boring, \nand it\'s kind of boring to write about. \nIf you write something interesting, it\'s probably wrong.',
+    author: '',
+    source: ''
+  },
+  {
+    content: 'Whenever there\'s a feeling of “someone should do something here”,\n there\'s a reasonable chance that this someone is you.',
+    author: '',
+    source: ''
+  },
+  {
+    content: 'Bad programmers worry about code. Good programmers worry about data structures and their relationships.',
+    author: '',
+    source: ''
+  }
+]
